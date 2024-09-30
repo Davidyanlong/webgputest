9a5814484c61ea0276dfe9586b03eacf989ec0be
yanlong.yang <yyl_love_you@126.com>
2024-09-30 14:04:41 +0800

    feat: index draw

--- src/demos/vertexBufferTriangles.ts
+++ src/demos/vertexBufferTriangles.ts
@@ -19,6 +19,7 @@ export class VertexBufferTriangles {
     private static changingVertexBuffer : GPUBuffer;
     private static staticVertexBuffer:GPUBuffer;
     private static vertexBuffer:GPUBuffer;
+    private static indexBuffer:GPUBuffer;
     private static vertexValues:Float32Array;
     private static numVertices:number;
     private static objectInfos:{scale:number}[] = [];
@@ -189,7 +190,7 @@ export class VertexBufferTriangles {
         // a typed array we can use to update the changingStorageBuffer
         VertexBufferTriangles.vertexValues = new Float32Array(changingVertexBufferSize / 4);
         // setup a storage buffer with vertex data
-        const { vertexData, numVertices } = VertexBufferTriangles.createCircleVertices({
+        const { vertexData,indexData, numVertices } = VertexBufferTriangles.createCircleVerticesIndex({
             radius: 0.5,
             innerRadius: 0.25,
         });
@@ -201,6 +202,13 @@ export class VertexBufferTriangles {
             usage: GPUBufferUsage.VERTEX | GPUBufferUsage.COPY_DST,
         });
         device.queue.writeBuffer(vertexBuffer, 0, vertexData);
+
+        const indexBuffer = VertexBufferTriangles.indexBuffer = device.createBuffer({
+            label: 'index buffer',
+            size: indexData.byteLength,
+            usage: GPUBufferUsage.INDEX | GPUBufferUsage.COPY_DST,
+          });
+          device.queue.writeBuffer(indexBuffer, 0, indexData);
 
 
         //#region  渲染队列参数
@@ -244,6 +252,7 @@ export class VertexBufferTriangles {
         pass.setVertexBuffer(0,VertexBufferTriangles.vertexBuffer);
         pass.setVertexBuffer(1, VertexBufferTriangles.staticVertexBuffer);
         pass.setVertexBuffer(2, VertexBufferTriangles.changingVertexBuffer);
+        pass.setIndexBuffer(VertexBufferTriangles.indexBuffer,'uint32');
 
         // 渲染多个对象
         let ndx = 0;
@@ -257,7 +266,7 @@ export class VertexBufferTriangles {
         VertexBufferTriangles.device.queue.writeBuffer(VertexBufferTriangles.changingVertexBuffer, 0, VertexBufferTriangles.vertexValues);
 
 
-        pass.draw(VertexBufferTriangles.numVertices, VertexBufferTriangles.kNumObjects);  // call our vertex shader 3 times
+        pass.drawIndexed(VertexBufferTriangles.numVertices, VertexBufferTriangles.kNumObjects);  // call our vertex shader 3 times
         pass.end();
 
         const commandBuffer = encoder.finish();
@@ -327,6 +336,80 @@ export class VertexBufferTriangles {
             numVertices,
         };
     }
+
+    private static  createCircleVerticesIndex({
+        radius = 1,
+        numSubdivisions = 24,
+        innerRadius = 0,
+        startAngle = 0,
+        endAngle = Math.PI * 2,
+      } = {}) {
+        // 2 vertices at each subdivision, + 1 to wrap around the circle.
+        const numVertices = (numSubdivisions + 1) * 2;
+        // 2 32-bit values for position (xy) and 1 32-bit value for color (rgb)
+        // The 32-bit color value will be written/read as 4 8-bit values
+        const vertexData = new Float32Array(numVertices * (2 + 1));
+        const colorData = new Uint8Array(vertexData.buffer);
+       
+        let offset = 0;
+        let colorOffset = 8;
+        const addVertex = (x:number, y:number, r:number, g:number, b:number) => {
+          vertexData[offset++] = x;
+          vertexData[offset++] = y;
+          offset += 1;  // skip the color
+          colorData[colorOffset++] = r * 255;
+          colorData[colorOffset++] = g * 255;
+          colorData[colorOffset++] = b * 255;
+          colorOffset += 9;  // skip extra byte and the position
+        };
+        const innerColor:[number, number, number] = [1, 1, 1];
+        const outerColor:[number, number, number] = [0.1, 0.1, 0.1];
+       
+        // 2 triangles per subdivision
+        //
+        // 0  2  4  6  8 ...
+        //
+        // 1  3  5  7  9 ...
+        for (let i = 0; i <= numSubdivisions; ++i) {
+          const angle = startAngle + (i + 0) * (endAngle - startAngle) / numSubdivisions;
+       
+          const c1 = Math.cos(angle);
+          const s1 = Math.sin(angle);
+       
+          addVertex(c1 * radius, s1 * radius, ...outerColor);
+          addVertex(c1 * innerRadius, s1 * innerRadius, ...innerColor);
+        }
+       
+        const indexData = new Uint32Array(numSubdivisions * 6);
+        let ndx = 0;
+       
+        // 1st tri  2nd tri  3rd tri  4th tri
+        // 0 1 2    2 1 3    2 3 4    4 3 5
+        //
+        // 0--2        2     2--4        4  .....
+        // | /        /|     | /        /|
+        // |/        / |     |/        / |
+        // 1        1--3     3        3--5  .....
+        for (let i = 0; i < numSubdivisions; ++i) {
+          const ndxOffset = i * 2;
+       
+          // first triangle
+          indexData[ndx++] = ndxOffset;
+          indexData[ndx++] = ndxOffset + 1;
+          indexData[ndx++] = ndxOffset + 2;
+       
+          // second triangle
+          indexData[ndx++] = ndxOffset + 2;
+          indexData[ndx++] = ndxOffset + 1;
+          indexData[ndx++] = ndxOffset + 3;
+        }
+       
+        return {
+          vertexData,
+          indexData,
+          numVertices: indexData.length,
+        };
+      }
 
 
 }
